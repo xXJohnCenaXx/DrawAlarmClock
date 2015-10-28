5af1ff7ee98db316da25c6f4926270292ab9869c
grclaeys <grclaeys@gmail.com>
2015-10-28 14:37:31 -0700

    more methods for buttons

--- app/src/main/java/com/example/seartho16/drawalarmclock/MainActivity.java
+++ app/src/main/java/com/example/seartho16/drawalarmclock/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,4 +35,17 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void addAlarm(View view) {
+
+    }
+
+    public void difficultyChange(View view) {
+
+    }
+
+    public void viewAlarms(View view) {
+
+    }
+    
 }
