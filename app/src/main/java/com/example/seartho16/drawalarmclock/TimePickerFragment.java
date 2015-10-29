cef158e4f18d385fa6935488f151430621059bdf
xXJohnCenaXx <thomas@potatoplanet.org>
2015-10-29 13:13:26 -0700

    things
    probably doesnt work

--- app/src/main/java/com/example/seartho16/drawalarmclock/TimePickerFragment.java
+++ app/src/main/java/com/example/seartho16/drawalarmclock/TimePickerFragment.java
@@ -7,6 +7,9 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+
 
 
 /**
@@ -22,7 +25,7 @@ public class TimePickerFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    public static TextView textview;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -63,10 +66,13 @@ public class TimePickerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_time_picker, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_time_picker,
+                container, false);
 
+        textview = (TextView) view.findViewById(R.id.textView);
+
+        return view;
+    }
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
