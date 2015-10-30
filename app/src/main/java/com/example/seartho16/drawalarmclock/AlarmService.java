beb8238d65bf63a73e932d507883c39ca254ba5a
grclaeys <grclaeys@gmail.com>
2015-10-30 12:30:28 -0700

    thread in a service

--- app/src/main/java/com/example/seartho16/drawalarmclock/AlarmService.java
+++ app/src/main/java/com/example/seartho16/drawalarmclock/AlarmService.java
@@ -3,6 +3,7 @@ package com.example.seartho16.drawalarmclock;
 import android.app.IntentService;
 import android.content.Intent;
 import android.content.Context;
+import android.media.AudioManager;
 
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
@@ -11,7 +12,7 @@ import android.content.Context;
  * TODO: Customize class - update intent actions, extra parameters and static
  * helper methods.
  */
-public class AlarmService extends IntentService {
+public class AlarmService extends IntentService implements Runnable {
     // TODO: Rename actions, choose action names that describe tasks that this
     // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
     private static final String ACTION_FOO = "com.example.seartho16.drawalarmclock.action.FOO";
@@ -43,42 +44,49 @@ public class AlarmService extends IntentService {
      * @see IntentService
      */
     // TODO: Customize helper method
-    public static void startActionBaz(Context context, String param1, String param2) {
-        Intent intent = new Intent(context, AlarmService.class);
-        intent.setAction(ACTION_BAZ);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
-        context.startService(intent);
-    }
+//    public static void startActionBaz(Context context, String param1, String param2) {
+//        Intent intent = new Intent(context, AlarmService.class);
+//        intent.setAction(ACTION_BAZ);
+//        intent.putExtra(EXTRA_PARAM1, param1);
+//        intent.putExtra(EXTRA_PARAM2, param2);
+//        context.startService(intent);
+//    }
 
     public AlarmService() {
         super("AlarmService");
     }
 
+    public void run() {}
     @Override
     protected void onHandleIntent(Intent intent) {
-        if (intent != null) {
-            final String action = intent.getAction();
-            if (ACTION_FOO.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionFoo(param1, param2);
-            } else if (ACTION_BAZ.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionBaz(param1, param2);
+//        if (intent != null) {
+//            final String action = intent.getAction();
+//            if (ACTION_FOO.equals(action)) {
+//                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
+//                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
+//                handleActionFoo(param1, param2);
+//            } else if (ACTION_BAZ.equals(action)) {
+//                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
+//                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
+//                handleActionBaz(param1, param2);
+//            }
+//        }
+        new Thread(new Runnable() {
+            public void run() {
+                
             }
-        }
+        }).start();
     }
+
 
     /**
      * Handle action Foo in the provided background thread with the provided
      * parameters.
      */
-    private void handleActionFoo(String param1, String param2) {
-        // TODO: Handle action Foo
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
+//    private void handleActionFoo(String param1, String param2) {
+//        // TODO: Handle action Foo
+//        throw new UnsupportedOperationException("Not yet implemented");
+//    }
 
     /**
      * Handle action Baz in the provided background thread with the provided
